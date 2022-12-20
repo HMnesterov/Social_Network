cc160c2be8fc8f24be087b8bb149bf9c42d16fbb
BenitoSwaggolini <profie_player@mail.ru>
2022-12-20 16:57:56 +0500

    friendship model

--- src/friendship/models.py
+++ src/friendship/models.py
@@ -2,16 +2,22 @@ from django.db import models
 
 from user.models import Person
 
+
 class Friendship(models.Model):
-    from_user = models.OneToOneField(Person, on_delete=models.CASCADE, verbose_name='Sender')
-    to_user = models.OneToOneField(Person, on_delete=models.CASCADE, verbose_name='Receiver')
+    from_user = models.OneToOneField(Person, on_delete=models.CASCADE, verbose_name='Sender',
+                                     related_name='from_one_user')
+    to_user = models.OneToOneField(Person, on_delete=models.CASCADE, verbose_name='Receiver',
+                                   related_name='to_another_user')
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
     def __str__(self):
-        return f"{self.person_id} wants to be your friend!"
+        return f"{self.from_user} wants to be your friend!"
 
     def return_time_created(self):
         return f"This application has been sent at {self.time_created}"
 
+    def get_sender_id(self):
+        return self.from_user.pk
 
-
+    def get_receiver_id(self):
+        return self.to_user.pk
