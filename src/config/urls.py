086306f1a855ae074a33f52f3b57a1c725f8be5f
BenitoSwaggolini <profie_player@mail.ru>
2022-12-20 17:16:31 +0500

    urls for friendship request

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
+from friendship import urls
 from user import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include(urls))
+    path('user/', include(urls)),
+    path('friendship/', include(urls)),
 ]
 
 if settings.DEBUG:
---
+++ src/friendship/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import send_friendship_request, accept_friendship_request, reject_friendship_request, remove_friendship_request
+
+urlpatterns = [
+
+    path('send_friendship_req/<int:id>', send_friendship_request),
+    path('accept_friendship/<int:id>', accept_friendship_request),
+    path('reject_friendship/<int:id>', reject_friendship_request),
+    path('remove_friendship_req/<int:id>', remove_friendship_request)
+]
