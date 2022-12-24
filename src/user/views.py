e8d6061f7883ad39e2656ad2014c20e448b3e6e8
BenitoSwaggolini <profie_player@mail.ru>
2022-12-24 22:09:04 +0500

    fixed auth bug, sidebar links were added

--- src/user/views.py
+++ src/user/views.py
@@ -8,7 +8,7 @@ from .forms import RegisterUserForm, LoginUserForm
 
 def register(request):
     if request.user.is_authenticated:
-        return HttpResponse('You already have an account!')
+        return redirect('user_profile', id=request.user.id)
     form = RegisterUserForm()
     if request.method == 'POST':
 
@@ -17,24 +17,24 @@ def register(request):
             user = form.save()
             login(request=request, user=user)
 
-            return HttpResponse('well done')
+            return redirect('user_profile', id=user.id)
 
     return render(request, 'user/register.html', {'form': form})
 
 
 def authorization(request):
     if request.user.is_authenticated:
-        return HttpResponse('You already in your account!')
+        return redirect('user_profile', id=request.user.id)
     form = LoginUserForm()
 
     if request.method == "POST":
 
-        form = LoginUserForm(request.POST, request.FILES)
+        form = LoginUserForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request=request, user=user)
 
-            return HttpResponse('success')
+            return redirect('user_profile', id=user.id)
 
     return render(request, 'user/register.html', {'form': form})
 
